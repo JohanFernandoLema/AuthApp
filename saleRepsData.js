ce43724da0a7be18cc5f0384cd881457af9f2ce9
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-25 22:39:24 -0400

    sales team info created - in future it will be used as db data

---
+++ saleRepsData.js
@@ -0,0 +1,34 @@
+const salesTeam = [
+  {
+    id: 1,
+    name: 'Johan Lema',
+    email: 'johan@pluscommunications.ca',
+    languages: ['spanish', 'english'],
+    phone: '(647) 581 - 6217',
+    picture: 'images/johan.pgn',
+  },
+  {
+    id: 2,
+    name: 'Catalina',
+    email: 'catalina@pluscommunications.ca',
+    languages: ['spanish', 'english'],
+    phone: '(437) 328 - 0008',
+    picture: 'images/catalina.pgn',
+  },
+  {
+    id: 3,
+    name: 'Dilnaz Virk',
+    email: 'dilnaz@pluscommunications.ca',
+    languages: ['english', 'hindi'],
+    phone: '(437) 328 - 0008',
+    picture: 'images/dilnaz.pgn',
+  },
+  {
+    id: 4,
+    name: 'Bansari',
+    email: 'bansari@pluscommunications.ca',
+    languages: ['english', 'hindi'],
+    phone: '(437) 328 - 0008',
+    picture: 'images/bansari.pgn',
+  },
+]
