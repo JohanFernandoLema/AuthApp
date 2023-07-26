e66216c66a098e512ddf18fdc28c0eb0ab34088b
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-25 22:17:53 -0400

    path switched in sales module due to error while loading

--- Routes/salesRep.jsx
+++ Routes/salesRep.jsx
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/salesTeam', (req, res) => {
+router.get('/', (req, res) => {
   res.send("You've reached our team")
 })
 
