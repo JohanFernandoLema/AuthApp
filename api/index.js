87f3e8a86a925ff0abe16296750eb196fbba1764
JohanFernandoLema <johan.13fernando@gmail.com>
2023-08-15 21:35:02 -0400

    connection stablished with the DB - .env and . gitigore files created

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
+// FRAMEWORKS REQUIRED
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const PORT = 7000
 
+// CONNECTING TO DATABASE
+const cloudDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO.URI)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 app.listen(PORT, () => {
+  cloudDB()
   console.log(`App running successfully on port: ${PORT}`)
 })
