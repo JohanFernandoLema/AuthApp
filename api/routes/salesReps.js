a09415af1579029af3f22efc69b954247894a356
JohanFernandoLema <johan.13fernando@gmail.com>
2023-08-23 21:45:08 -0400

    update rep by its id created

--- api/routes/salesReps.js
+++ api/routes/salesReps.js
@@ -13,4 +13,20 @@ router.post('/', async (req, res) => {
   }
 })
 
+//UPDATE A SALES REP
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedRep = await Representative.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+    res.status(200).json(updatedRep)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 module.exports = router
