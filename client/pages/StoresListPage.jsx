3a88050330885c47e361d8bae183bb8873ce5f70
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-11 12:04:55 -0400

    page site being designed

--- client/pages/StoresListPage.jsx
+++ client/pages/StoresListPage.jsx
@@ -1,26 +1,25 @@
+import './storesListPage.scss'
 import stores from './mock_data'
 import { Link } from 'react-router-dom'
 const StoresPage = () => {
   return (
-    <div>
+    <div className="cards">
       {stores.map((store, index) => (
-        <div key={index}>
+        <div key={index} className="cardsLayout">
           <div>
             <Link to={`/stores-list/${store.name}`}>
               <h3>{store.location}</h3>
             </Link>
             <img src={store.picture} alt="image" />
-            <div>
-              <ul>
-                <li>{store.schedule[0]}</li>
-                <li>{store.schedule[1]}</li>
-                <li>{store.schedule[2]}</li>
-                <li>{store.schedule[3]}</li>
-                <li>{store.schedule[4]}</li>
-                <li>{store.schedule[5]}</li>
-                <li>{store.schedule[6]}</li>
-              </ul>
-            </div>
+            <ul>
+              <li>{store.schedule[0]}</li>
+              <li>{store.schedule[1]}</li>
+              <li>{store.schedule[2]}</li>
+              <li>{store.schedule[3]}</li>
+              <li>{store.schedule[4]}</li>
+              <li>{store.schedule[5]}</li>
+              <li>{store.schedule[6]}</li>
+            </ul>
           </div>
         </div>
       ))}
