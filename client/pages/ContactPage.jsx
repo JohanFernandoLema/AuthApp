61ec47bd3d16cc439bbba8b047223d617d095035
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-10 22:13:50 -0400

    basic pages' layouts designed - mock contact page started to being created

--- client/pages/ContactPage.jsx
+++ client/pages/ContactPage.jsx
@@ -1,4 +1,33 @@
 const ContactPage = () => {
-  return <div>You can contact me here</div>
+  return (
+    <div className="formPage">
+      <p>
+        Let us know <b> how can we help you?</b>
+      </p>
+      <div className="contactForm">
+        <form action="/">
+          <label htmlFor="userName">
+            Name:
+            <input type="text" name="" id="" placeholder="Input your name" />
+          </label>
+          <label htmlFor="userEmail">
+            E-mai:
+            <input type="email" name="" id="" placeholder="Input your e-mail" />
+          </label>
+          <label htmlFor="userComment">
+            Comments:
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Something you want to share"
+            ></textarea>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 export default ContactPage
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,23 @@
-import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import HomePage from '../pages/homePage'
 import AboutPage from '../pages/aboutPage'
 import ContactPage from '../pages/contactPage'
 import StoresPage from '../pages/storesPage'
+import './app.scss'
 function App() {
   return (
     <div>
       <BrowserRouter>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/stores-information" element={<StoresPage />} />
-          <Route path="/contact-us" element={<ContactPage />} />
-        </Routes>
+        <div className="pagesDisplay">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/stores-information" element={<StoresPage />} />
+            <Route path="/contact-us" element={<ContactPage />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   )
