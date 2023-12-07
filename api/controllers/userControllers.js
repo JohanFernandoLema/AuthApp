7fa153ce15b7727947ab41bcadd3bc89ae3c165a
JohanFernandoLema <johan.13fernando@gmail.com>
2023-12-06 21:55:38 -0500

    uri for registering users created

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,7 +1,20 @@
 import User from '../models/userModel.js'
 
+// Register User
 const homePage = async (req, res) => {
-  res.send(200).json({ message: 'Home page endpoint' })
+  const { email, password } = req.body
+
+  const validEmail = await User.findOne({ email })
+
+  if (validEmail) {
+    res.status(401).json({ message: 'Log in using valid credentials' })
+  }
+
+  const user = await User.create({
+    email: email,
+  })
+
+  res.status(200).json(user)
 }
 
 export default homePage
