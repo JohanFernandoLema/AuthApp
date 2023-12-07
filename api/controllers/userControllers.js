79f249cdf56740f57bc9c426c3d758788c525f93
JohanFernandoLema <johan.13fernando@gmail.com>
2023-12-07 09:04:12 -0500

    routes for deleting, getting all users and logging in added

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,22 +1,52 @@
 import User from '../models/userModel.js'
 
 // Register User
-const homePage = async (req, res) => {
+const registerUser = async (req, res) => {
   const { name, email, password } = req.body
 
   const validEmail = await User.findOne({ email })
 
-  if (validEmail) {
-    res.status(401).json({ message: 'Log in using valid credentials' })
+  if (!validEmail) {
+    const user = await User.create({
+      name: name,
+      email: email,
+    })
+    return res.status(200).json(user)
+  } else {
+    return res.status(401).json({ message: 'Log in using valid credentials' })
   }
-
-  const user = await User.create({
-    name: name,
-    email: email,
-    password: password,
-  })
-
-  res.status(200).json(user)
 }
 
-export default homePage
+// Log In User
+
+const logIn = async (req, res) => {
+  const { email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(200).json({
+      email: email,
+    })
+  } else {
+    res
+      .status(401)
+      .json({ message: 'Please make sure you are using valid credentials' })
+  }
+}
+
+// Delete User
+
+const deleteUser = async (req, res) => {
+  await User.findByIdAndDelete(req.params.id)
+  res.status(200).json({ message: 'User has been removed successfully' })
+}
+
+// Get User List
+
+const userList = async (req, res) => {
+  const users = await User.find()
+  res.status(200).json(users)
+}
+
+export { registerUser, logIn, deleteUser, userList }
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,8 +1,19 @@
 import express from 'express'
-import homePage from '../controllers/userControllers.js'
+import {
+  registerUser,
+  logIn,
+  deleteUser,
+  userList,
+} from '../controllers/userControllers.js'
 
 const router = express.Router()
 
-router.post('/register', homePage)
+router.post('/register', registerUser)
+
+router.post('/login', logIn)
+
+router.delete('/register/:id', deleteUser)
+
+router.get('/users', userList)
 
 export default router
