3c3063a758344e297b6f0d9f37add6b42b0cf575
JohanFernandoLema <johan.13fernando@gmail.com>
2023-12-13 10:19:48 -0500

    utils folder created with verify token on it - it will allow us to verify that the users are the real one

---
+++ api/utils/verifyToken.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+
+const protect = async (req, res, next) => {
+  const token = req.token.jwt
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.MY_SECRET)
+      req.user = await User.findById(decoded.email)
+
+      next()
+    } catch (error) {
+      res.status(401).json({ message: 'Not authorized, invalid token' })
+    }
+  } else {
+    res
+      .status(401)
+      .json({ message: 'Not authorized, no token to be validated' })
+  }
+}
+
+export { protect }
