2239a49025107ff144dfc0b7aaf76a304750258d
JohanFernandoLema <johan.13fernando@gmail.com>
2023-12-13 18:03:05 -0500

    protect component sent towards the routes - now user's profile is protected only those who are validated can access to this info

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -5,7 +5,10 @@ import {
   deleteUser,
   userList,
   logOut,
+  getUserProfile,
+  updateUserProfile,
 } from '../controllers/userControllers.js'
+import { protect } from '../utils/verifyToken.js'
 
 const router = express.Router()
 
@@ -14,6 +17,11 @@ router.post('/register', registerUser)
 router.post('/login', logIn)
 
 router.post('/logOut', logOut)
+
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 router.delete('/register/:id', deleteUser)
 
