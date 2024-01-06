82a71d6425c559c0bca44f9b0440065a33a8a608
JohanFernandoLema <johan.13fernando@gmail.com>
2024-01-05 20:19:42 -0500

    new routes added towards userContoller file - in this file we while find the endpoints where the api will go directed to

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,22 @@
 import asyncHandler from 'express-async-handler'
 
 const authUser = asyncHandler((req, res) => {
-  res.status(401)
-  throw new Error('Something went wrong')
   res.status(200).json({ message: 'Auth User' })
 })
 
-export { authUser }
+const registerUser = asyncHandler((req, res) => {
+  res.status(200).json({ message: 'Register User' })
+})
+
+const logoutUser = asyncHandler((req, res) => {
+  res.status(200).json({ message: 'Logout User' })
+})
+
+const getUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({ message: 'User profile' })
+})
+
+const updateUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({ message: 'Update user profile' })
+})
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
