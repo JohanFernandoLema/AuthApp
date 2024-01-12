fd285b193fa1f5257d605119a250edeb0f9233c7
JohanFernandoLema <johan.13fernando@gmail.com>
2024-01-11 21:47:23 -0500

    auth middleware file created - only authenticated users will be allowed to make changes in their account - otherwise will get a prompt error message

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
 dotenv.config()
+import cookieParser from 'cookie-parser'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 import connectDB from './config/db.js'
 const port = process.env.PORT || 5000
 
 connectDB()
+
 const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 
