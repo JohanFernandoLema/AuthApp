5e76c7c75dd6213d0c33fe97dc87bf9f383fd212
JohanFernandoLema <johan.13fernando@gmail.com>
2024-01-24 22:20:41 -0500

    register page is working already - it still needs some adjustments such as allowing member to re-direct to the profile after they have signed up

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,17 +1,45 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer.jsx'
+import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
+import Loader from '../components/Loader.jsx'
+import { useRegisterMutation } from '../slices/usersApiSlice.js'
+import { setCredentials } from '../slices/authSlices.js'
 
 const RegisterScreen = () => {
-  const { name, setName } = useState('')
-  const { email, setEmail } = useState('')
-  const { password, setPassword } = useState('')
-  const { confirmPassword, setConfirmPassword } = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { userInfo } = useSelector((state) => state.auth)
+
+  const [register, { isLoading }] = useRegisterMutation()
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/')
+    }
+  }, [navigate, userInfo])
 
   const submitHandler = async (e) => {
     e.preventDefault()
-    console.log('submit')
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match')
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap()
+        dispatch(setCredentials({ ...res }))
+        navigate('/')
+      } catch (err) {
+        toast.error(err?.data?.message || err.error)
+      }
+    }
   }
   return (
     <FormContainer>
@@ -58,6 +86,7 @@ const RegisterScreen = () => {
           ></Form.Control>
         </Form.Group>
 
+        {isLoading && <Loader />}
         <Button type="submit" varian="primary" className="mt-3">
           Sign Up
         </Button>
